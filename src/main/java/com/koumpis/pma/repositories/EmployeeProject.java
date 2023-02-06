21d12791c1ea3c70fb966170b9dd58f25b14dbe8
Button99 <56029580+Button99@users.noreply.github.com>
2023-02-06 18:22:09 +0200

    Created EmployeeRepository

---
+++ src/main/java/com/koumpis/pma/repositories/EmployeeProject.java
@@ -0,0 +1,7 @@
+package com.koumpis.pma.repositories;
+
+public interface EmployeeProject {
+    public String getFirstName();
+    public String getLastName();
+    public int getProjectCount();
+}
--- src/main/java/com/koumpis/pma/repositories/EmployeeRepository.java
+++ src/main/java/com/koumpis/pma/repositories/EmployeeRepository.java
@@ -1,6 +1,7 @@
 package com.koumpis.pma.repositories;
 
 import com.koumpis.pma.entities.Employee;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -8,4 +9,7 @@ import java.util.List;
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
     @Override
     List<Employee> findAll();
+
+    @Query(nativeQuery = true, value = "SELECT e.first_name as firstName, e.last_name as lastName, COUNT(pe.employee_id) as projectCount FROM employee e left join project_employee pe ON pe.employee_id = e.employee_id GROUP BY e.first_name, e.last_name ORDER BY 3 DESC;")
+    public List<EmployeeProject> employeeProjects();
 }
