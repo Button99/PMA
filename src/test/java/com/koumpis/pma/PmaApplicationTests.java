cec6e1dfb37be9a0529742f9b3077104f7450340
Button99 <56029580+Button99@users.noreply.github.com>
2023-02-08 13:42:51 +0200

    Schema del

--- src/test/java/com/koumpis/pma/PmaApplicationTests.java
+++ src/test/java/com/koumpis/pma/PmaApplicationTests.java
@@ -1,13 +1,20 @@
 package com.koumpis.pma;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest()
 class PmaApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+
+
 
 }
