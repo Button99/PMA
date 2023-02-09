2bffde17d37d21d273addeff1634347951e73972
Button99 <56029580+Button99@users.noreply.github.com>
2023-02-09 16:14:10 +0200

    CC

--- src/main/java/com/koumpis/pma/repositories/EmployeeRepository.java
+++ src/main/java/com/koumpis/pma/repositories/EmployeeRepository.java
@@ -1,15 +1,18 @@
 package com.koumpis.pma.repositories;
 
 import com.koumpis.pma.entities.Employee;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+
 
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
     @Override
     List<Employee> findAll();
 
     @Query(nativeQuery = true, value = "SELECT e.first_name as firstName, e.last_name as lastName, COUNT(pe.employee_id) as projectCount FROM employee e left join project_employee pe ON pe.employee_id = e.employee_id GROUP BY e.first_name, e.last_name ORDER BY 3 DESC;")
-    public List<EmployeeProject> employeeProjects();
+    List<EmployeeProject> employeeProjects();
 }
